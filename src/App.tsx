6eb91d5a21c4dbc6ba0e4a0313a8a3cf2ac4c7ed
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-09-10 22:50:01 +0900

    feat: Bootstarp導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import "./App.css";
+import { Breadcrumb } from "./components/Breadcrumb";
 import { UserDetail } from "./UserDetail";
 
 function App() {
   return (
     <>
+      <Breadcrumb />
       <UserDetail />
     </>
   );
---
+++ src/components/Breadcrumb.tsx
@@ -0,0 +1,17 @@
+export const Breadcrumb = () => {
+  return (
+    <nav aria-label="breadcrumb">
+      <ol className="breadcrumb">
+        <li className="breadcrumb-item">
+          <a href="#">Home</a>
+        </li>
+        <li className="breadcrumb-item">
+          <a href="#">Library</a>
+        </li>
+        <li className="breadcrumb-item">
+          <a href="#">Date</a>
+        </li>
+      </ol>
+    </nav>
+  );
+};

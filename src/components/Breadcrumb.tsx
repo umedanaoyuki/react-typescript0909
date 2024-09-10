7b1cb2a3a8fa3ec063e16f76611330494f19fc99
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-09-10 22:52:30 +0900

    feat: Fragmentの追加

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -1,17 +1,20 @@
 export const Breadcrumb = () => {
   return (
-    <nav aria-label="breadcrumb">
-      <ol className="breadcrumb">
-        <li className="breadcrumb-item">
-          <a href="#">Home</a>
-        </li>
-        <li className="breadcrumb-item">
-          <a href="#">Library</a>
-        </li>
-        <li className="breadcrumb-item">
-          <a href="#">Date</a>
-        </li>
-      </ol>
-    </nav>
+    <>
+      <p className="">パンくずリスト</p>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <a href="#">Home</a>
+          </li>
+          <li className="breadcrumb-item">
+            <a href="#">Library</a>
+          </li>
+          <li className="breadcrumb-item">
+            <a href="#">Date</a>
+          </li>
+        </ol>
+      </nav>
+    </>
   );
 };

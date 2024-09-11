bea201b60f591824d4e893b0149b9f9ee24ce815
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-09-12 03:03:16 +0900

    バックエンドからデータを取得

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,11 +1,22 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
+type Post = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
 export const PostList = () => {
+  const [postList, setPostList] = useState<Post[]>([]);
   const [userId, setUserId] = useState(1);
 
   useEffect(() => {
-    console.log("PostsList");
-  }, [userId]);
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      setPostList(res.data as Post[]);
+    });
+  }, []);
 
   return (
     <div>
@@ -18,6 +29,28 @@ export const PostList = () => {
           onChange={(e) => setUserId(Number(e.target.value))}
         />
       </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">ユーザーid</th>
+            <th scope="col">タイトル</th>
+            <th scope="col">内容</th>
+          </tr>
+        </thead>
+        <tbody>
+          {postList
+            .filter((post) => post.userId === userId)
+            .map((post) => (
+              <tr key={post.id}>
+                <td>{post.id}</td>
+                <td>{post.userId}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 };

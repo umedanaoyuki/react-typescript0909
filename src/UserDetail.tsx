526915331ba8910fc7739c24b8a20534aee23847
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-09-12 00:33:57 +0900

    feat: オブジェクトのstate管理

--- src/UserDetail.tsx
+++ src/UserDetail.tsx
@@ -1,10 +1,37 @@
+import { useState } from "react";
+
+type User = {
+  firstName: string;
+  lastName: string;
+  loginCount: number;
+};
+
 export const UserDetail = () => {
-  const age = 25;
+  const [user, setUser] = useState<User>({
+    firstName: "sample",
+    lastName: "太郎",
+    loginCount: 0,
+  });
+
+  const handleLogin = () => {
+    const newCount = user.loginCount + 1;
+    setUser({
+      ...user,
+      loginCount: newCount,
+    });
+  };
 
   return (
     <div>
-      <p>太郎</p>
-      <p>{age}歳</p>
+      <p>
+        {user.firstName} {user.lastName}
+      </p>
+      <p>ログイン回数: {user.loginCount} </p>
+      <div>
+        <button className="btn btn-success" onClick={handleLogin}>
+          ログイン
+        </button>
+      </div>
     </div>
   );
 };
